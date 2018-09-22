a24a8cb1f4d1fbfb238ed57085983a86e867e704
Outhai SAIOUDOM <os55555gster@gmail.com>
2018-09-22 18:16:11 +0700

    Update and rename install_k8s.sh to install_k8s_master_node.sh

--- install_k8s_master_node.sh
+++ install_k8s_master_node.sh
@@ -19,3 +19,11 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 sudo bash -c 'echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list'
 sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
+sysctl net.bridge.bridge-nf-call-iptables=1
+
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+#INSTALL SDN
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/v0.8.0/Documentation/kube-flannel-rbac.yml
+kubeadm init --apiserver-advertise-address=0.0.0.0 --pod-network-cidr=10.244.0.0/16 
+kubectl taint nodes --all node-role.kubernetes.io/master-
