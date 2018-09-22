e6e3e706ceb2bc927b475338a01c492988008925
Outhai SAIOUDOM <os55555gster@gmail.com>
2018-09-22 22:22:58 +0700

    Update install_k8s_slave_node.sh

--- install_k8s_slave_node.sh
+++ install_k8s_slave_node.sh
@@ -14,9 +14,14 @@ sudo groupadd docker
 sudo usermod -aG docker $USER
 sudo systemctl enable docker
 
+
+sudo systemctl daemon-reload
+sudo systemctl restart docker
+
 #sudo apt-get update && apt-get install -y apt-transport-https curl
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 sudo bash -c 'echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list'
 sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
 sysctl net.bridge.bridge-nf-call-iptables=1
+
